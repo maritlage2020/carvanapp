b6fa2285c1bb5b26f96daacfe58fb0506df0d1a7
maritlage2020 <maritlage2020.amana@gmail.com>
2019-11-28 15:58:24 +0900

    [start]3-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,6 +3,7 @@ class BlogsController < ApplicationController
   end
 
   def show
+  	@blog  = Blog.find(params[:id])
   end
 
   def index
@@ -20,8 +21,14 @@ class BlogsController < ApplicationController
   end
 
   def edit
+  	@blog = Blog.find(params[:id])
   end
 
+  def update
+  	blog = Blog.find(params[:id])
+  	blog.update(blog_params)
+  	redirect_to blog_path(blog)
+  end
 private
   def blog_params
   	params.require(:blog).permit(:title, :category, :body)
